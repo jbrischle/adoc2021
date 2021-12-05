970c4b5162cb36fc68092d62665f5e7819566e88
jbrischle <34125672+jbrischle@users.noreply.github.com>
2021-12-05 18:01:34 +0100

    add part two of puzzle one

--- puzzle-1-test.rb
+++ puzzle-1-test.rb
@@ -6,4 +6,9 @@ class PuzzleOneTest < Test::Unit::TestCase
     input = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
     assert_equal 7, PuzzleOne.increased_value(input)
   end
+
+  def test_part_two
+    input = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+    assert_equal 5, PuzzleOne.increased_value_sliding_window(input)
+  end
 end
--- puzzle-1.rb
+++ puzzle-1.rb
@@ -9,7 +9,19 @@ class PuzzleOne
     return increased_count
   end
 
+  def self.increased_value_sliding_window(input)
+    return unless input.is_a?(Array)
+
+    adjusted_input = []
+    input.each_with_index do |val, index|
+      if input[index + 1] && input[index + 2]
+        adjusted_input.push(val + input[index + 1] + input[index + 2])
+      end
+    end
+    return PuzzleOne.increased_value(adjusted_input)
+  end
 end
 
 input = File.readlines('puzzle-1-data.txt').map(&:to_i)
 puts "Answer part 1: #{PuzzleOne.increased_value(input)}"
+puts "Answer part 2: #{PuzzleOne.increased_value_sliding_window(input)}"
