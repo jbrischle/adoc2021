4f321d4bf49be946bacda3ce9c6e734dbc222f34
jbrischle <34125672+jbrischle@users.noreply.github.com>
2021-12-05 17:46:10 +0100

    create a class with method for puzzle 1

--- puzzle-1.rb
+++ puzzle-1.rb
@@ -1,11 +1,15 @@
-input = File.readlines("puzzle-1-data.txt").map(&:to_i)
-increased_count = 0
+class PuzzleOne
+  def self.increased_value(input)
+    return unless input.is_a?(Array)
 
-input.each_with_index do |val, index|
-  if input[index + 1] && (input[index + 1] - val).positive?
-    increased_count += 1
+    increased_count = 0
+    input.each_with_index do |val, index|
+      increased_count += 1 if input[index + 1] && (input[index + 1] - val).positive?
+    end
+    return increased_count
   end
+
 end
 
-puts increased_count
-
+input = File.readlines('puzzle-1-data.txt').map(&:to_i)
+puts "Answer part 1: #{PuzzleOne.increased_value(input)}"
