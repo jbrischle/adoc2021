9c28f83ca3a68618ca1b0a161f50f58eb418620d
jbrischle <34125672+jbrischle@users.noreply.github.com>
2021-12-05 16:17:59 +0100

    auto fix by ide

--- puzzle-1.rb
+++ puzzle-1.rb
@@ -1,11 +1,11 @@
 input = File.readlines("puzzle-1-data.txt").map(&:to_i)
 increased_count = 0
 
-input.each_with_index { |val, index|
+input.each_with_index do |val, index|
   if input[index + 1] && (input[index + 1] - val).positive?
     increased_count += 1
   end
-}
+end
 
 puts increased_count
 
