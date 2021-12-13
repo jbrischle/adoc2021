4641b4f5d871aaa7e9128da818155285cbd0e810
jbrischle <34125672+jbrischle@users.noreply.github.com>
2021-12-14 00:16:58 +0100

    finish puzzle 4 a

--- puzzle-4-a.rb
+++ puzzle-4-a.rb
@@ -1,56 +1,76 @@
-class PuzzleFourA
+require 'matrix'
 
-  @number_of_columns = 0
-  @number_of_lines = 0
+class PuzzleFourA
 
   def self.bingo(drawn_numbers, bingo_boards)
     return unless drawn_numbers.is_a?(Array)
     return unless bingo_boards.is_a?(Array)
 
     boards = create_bingo_boards(bingo_boards)
-
     drawn_numbers.each do |drawn_number|
-      stamp_boards(boards, drawn_number)
+      boards.each do |board_key, board|
+        boards[board_key] = stamp_board(board, drawn_number)
+        bingo = check_bingo(board)
+        if bingo
+          sum_up_unmarked_values = sum_up_unmarked_values(board)
+          return sum_up_unmarked_values * drawn_number
+        end
+      end
     end
-    pp boards
   end
 
   private
 
-  def self.stamp_boards(boards, drawn_number)
-    boards.each do |_board_key, board_lines|
-      board_lines.each do |_line_key, line_values|
-        line_values.each do |item|
-          item['marked'] = true if item['number'].eql? drawn_number.to_s
-        end
-      end
+  def self.sum_up_unmarked_values(board)
+    sum = 0
+    board.each_with_index do |value, index|
+      sum += value[0].to_i if value[1] != true
     end
+    sum
+  end
+
+  def self.check_bingo(board)
+    row_marks = board.row(0).map { |value| value[1] }.uniq
+    return true if row_marks.length == 1 && row_marks[0] == true
+
+    column_marks = board.column(0).map { |value| value[1] }.uniq.inspect
+    return true if column_marks.length == 1 && column_marks[0] == true
+  end
+
+  def self.stamp_board(board, drawn_number)
+    rows = []
+    row = []
+    current_row = 0
+
+    board.each_with_index do |value, index|
+      if current_row != index
+        rows.push(row)
+        current_row = index
+        row = []
+      end
+      value[1] = true if value[0].eql? drawn_number.to_s
+      row.push(value)
+    end
+    rows.push(row)
+    Matrix[*rows]
   end
 
   def self.create_bingo_boards(bingo_boards)
-    line_counter = 0
-    board = {}
-    boards_counter = 0
     boards = {}
+    boards_counter = 0
+    rows = []
 
     bingo_boards.each do |line|
-      numbers = line.gsub(/\s+/m, ' ').strip.split(' ')
-      @number_of_columns = numbers.length
-      if numbers.length <= 0
+      if line.strip.empty?
+        boards[boards_counter] = Matrix[*rows]
         boards_counter += 1
-        @number_of_lines = line_counter
-        line_counter = 0
+        rows = []
       else
-        array = []
-        numbers.each do |number|
-          array.push({ 'number' => number, 'marked' => false })
-        end
-
-        board["line#{line_counter}"] = array
-        boards["board#{boards_counter}"] = board
-        line_counter += 1
+        numbers = line.gsub(/\s+/m, ' ').strip.split(' ')
+        rows.push(numbers.map { |num| [num, false] })
       end
     end
+    boards[boards_counter] = Matrix[*rows]
     boards
   end
 end
